f4fc99a470e9ff1dbc3e7c88dd198fce816db1ba
EPSA <120899038+ePSA-eJya@users.noreply.github.com>
2026-04-19 18:17:51 +0530

    repos changes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "notification-pref/internal/app"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// import "Notification_Preferences/internal/app"
 
 func main() {
-	app.Start() // Call server.go
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found")
+		log.Panicln("No .env file found")
+	}
+
+	uri := os.Getenv("MONGO_URI")
+
+	fmt.Println(uri)
+	// app.Start() // Call server.go
 }
